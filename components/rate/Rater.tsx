dd9e1a7152c04c9e2ec587f43eda265fb7ef9e9e
Kaio <kaiolucas1812@gmail.com>
2025-02-28 03:27:25 -0300

    Update Rater.tsx

--- components/rate/Rater.tsx
+++ components/rate/Rater.tsx
@@ -166,7 +166,9 @@ export default function Rater({
                     favorite: false,
                 }));
                 setRatings(initialRatings);
-                setContent("<p></p>");
+                setContent(
+
+{"type": "doc", "content": [{"type": "paragraph", "content": [{"text": "", "type": "text"}]}]});
             }
         };
 

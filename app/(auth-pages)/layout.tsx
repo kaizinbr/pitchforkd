e3f37e9ce8710669d7b6bae0ed45307f907a7dd0
kaizin <kaiolucas1812@gmail.com>
2025-12-29 23:13:16 -0300

    talk to u later later

--- app/(auth-pages)/layout.tsx
+++
@@ -1,9 +0,0 @@
-export default async function Layout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
-  return (
-    <div className="max-w-2xl w-full flex flex-col gap-12 min-h-screen items-start">{children}</div>
-  );
-}
